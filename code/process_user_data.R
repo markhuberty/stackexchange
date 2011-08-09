beeeddd7728a9072464b79f8bf3892a5e360b0c7
Your Name <mark.huberty@gmail.com>
2011-08-09 16:28:38 -0400

    Added code for the yahoo placemaker conversion

--- code/process_user_data.R
+++ code/process_user_data.R
@@ -1,31 +1,82 @@
 
 if(grepl("mac.binary", .Platform$pkgType, fixed=TRUE))
   {
-    setwd("~/Documents/Research/Papers/stackoverflow")
+    setwd("~/Documents/Research/Papers/stackexchange")
   }else{
-    setwd("~/Documents/stackoverflow")
+    setwd("~/Documents/stackexchange")
   }
 library(XML)
 library(foreach)
 
-users.xml <- xmlTreeParse("users.xml", useInternalNodes=TRUE)
+states <- read.csv("./data/states.csv")
+
+users.file <-
+  "./data/stack-dump/stack_dump_apr2011/Content/stackoverflow/stackoverflow_dump_apr2011/users.xml"
+
+users.xml <- xmlTreeParse(users.file, useInternalNodes=TRUE)
 
 users.list <- xmlToList(users.xml)
 
-users.df <- foreach(i=1:length(users.list), .combine="rbind") %do% {
+fields.to.take <- c("Id",
+                    "Reputation",
+                    "CreationDate",
+                    "DisplayName",
+                    "LastAccessDate",
+                    "WebsiteUrl",
+                    "Location",
+                    "UpVotes",
+                    "DownVotes"
+                    )
 
-  out <- c(users.list[[i]]$ID,
-           users.list[[i]]$Reputation,
-           users.list[[i]]$CreationDate,
-           users.list[[i]]$DisplayName,
-           users.list[[i]]$LastAccessDate,
-           users.list[[i]]$WebsiteUrl,
-           users.list[[i]]$Location,
-           users.list[[i]]$UpVotes,
-           users.list[[i]]$DownVotes
-           )
+N <- length(users.list)
+users.df <- foreach(i=1:N, .combine="rbind") %do% {
 
+  out <- users.list[[i]][fields.to.take]
+  
   return(out)
 
 
 }
+
+users.df <- as.data.frame(users.df
+                          )
+
+col.classes <- c("character",
+                 "integer",
+                 "character",
+                 "character",
+                 "character",
+                 "character",
+                 "character",
+                 "integer",
+                 "integer"
+                 )
+
+for(i in 1:ncol(users.df))
+  {
+
+    users.df[,i] <- as.character(users.df[,i])
+    class(users.df[,i]) <- col.classes[i]
+
+  }
+
+save(users.df,
+     file="./data/users.RData"
+     )
+
+users.df.location <- users.df[!is.na(users.df$Location),]
+
+locations <- unique(users.df.location$Location)
+locations <- tolower(locations)
+
+## Encode correctly for the POST request
+locations <- gsub(" ", "+", locations)
+locations <- gsub(",", "", locations)
+
+write.csv(locations, file="./data/unique.locations.csv")
+
+count.location <- tapply(rep(1, nrow(users.df.location)),
+                         tolower(users.df.location$Location),
+                         sum
+                         )
+                             
---
+++ code/yahoo_placemaker.py
@@ -0,0 +1,70 @@
+import urllib2
+import urllib
+import os
+import csv
+import re
+import time
+
+os.chdir('/home/markhuberty/Documents/stackexchange/data')
+baseurl = 'http://wherein.yahooapis.com/v1/document'
+apikey = 'D_LXiILV34FiY77NdugJQa._sV3kNPewot_0KsO.ZrNvX_iqWUmEGQ4UhCspZXzL6khK'
+documentType = 'text/plain'
+requests = []
+
+## Initiate the locations and read in the CSV file
+locations = []
+
+def load_locations(location_csv):
+    locations = []
+    with open(location_csv, 'rb') as f:
+        for row in csv.reader(f):
+            locations.append(row)
+    return locations
+
+def format_request(locations, apikey):
+    requests = []
+    for location in locations:
+        data = {'documentContent':location[1], 
+                'documentType':'text/plain', 
+                'appid':apikey
+                }
+        requests.append(data)
+    return requests
+        
+def geocode_request(locations, requests, baseurl, sleep_interval=1):
+    geocoded_location = []    
+    for l, r in zip(locations, requests):
+        encoded_request = urllib.urlencode(r)
+        req = urllib2.Request(baseurl, encoded_request)
+        response = urllib2.urlopen(req)
+        result = response.read()
+        geocoded_location.append({'location':l[1], 'result':result})
+        time.sleep(sleep_interval)
+    return(geocoded_location)
+
+## End function definition
+#################################################################
+
+## Get the locations and convert to encoded requests
+locations = load_locations('unique.locations.csv')
+requests = format_request(locations, apikey)
+
+#test_results = geocode_request(locations[1:10], requests[1:10], baseurl)
+geocoded_results = geocode_request(locations, requests, baseurl)
+
+
+## Write out the results to a csv file
+fieldnames = ('location', 'xml.location')
+with open('geocoded_results.csv', 'wt') as f:
+    writer = csv.DictWriter(f, fieldnames = fieldnames)
+    headers = dict( (n,n) for n in fieldnames)
+    writer.writerow(headers)
+    for result in geocoded_results:
+        writer.writerow({'location': result['location'],
+                         'xml.location': result['result']
+                         }
+                        )
+            
+quit()
+
+
