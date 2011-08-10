69de5b41e1be3f92f794007f0c62fe0d050f148b
Mark Huberty <mark.huberty@gmail.com>
2011-08-09 18:49:06 -0700

    Added error handling to the url get code

--- code/yahoo_placemaker.py
+++ code/yahoo_placemaker.py
@@ -33,12 +33,20 @@ def format_request(locations, apikey):
         
 def geocode_request(locations, requests, baseurl, sleep_interval=1):
     geocoded_location = []    
-    for l, r in zip(locations, requests):
-        encoded_request = urllib.urlencode(r)
-        req = urllib2.Request(baseurl, encoded_request)
-        response = urllib2.urlopen(req)
-        result = response.read()
-        geocoded_location.append({'location':l[1], 'result':result})
+    for l, r, n in zip(locations, requests, range(len(requests))):
+        print n
+        try:
+            encoded_request = urllib.urlencode(r)
+            req = urllib2.Request(baseurl, encoded_request)
+            response = urllib2.urlopen(req) 
+        except urllib2.URLError, e:
+            if hasattr(e, 'reason'):
+                print 'Reason', e.reason
+            elif hasattr(e, 'code'):
+                print 'Error code', e.code
+        else:
+            result = response.read()
+            geocoded_location.append({'location':l[1], 'result':result})
         time.sleep(sleep_interval)
     return(geocoded_location)
 
@@ -50,7 +58,7 @@ locations = load_locations('unique.locations.csv')
 requests = format_request(locations, apikey)
 
 #test_results = geocode_request(locations[1:10], requests[1:10], baseurl)
-geocoded_results = geocode_request(locations, requests, baseurl)
+geocoded_results = geocode_request(locations, requests, baseurl, sleep_interval=0.1)
 
 
 ## Write out the results to a csv file
