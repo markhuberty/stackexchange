9ce42c3bf4e74ae41e00d1c6bb447a41364b01da
Your Name <mark.huberty@gmail.com>
2011-08-12 09:51:33 -0400

    Uncommented the uniquify function

--- code/generate_sparse_tag_matrix.py
+++ code/generate_sparse_tag_matrix.py
@@ -40,21 +40,21 @@ conn.close()
 # # del tag_data
 
 # ## Taken from here: http://www.peterbe.com/plog/uniqifiers-benchmark
-# def uniquify(seq, idfun=None):  
-#     # order preserving 
-#     if idfun is None: 
-#         def idfun(x): return x 
-#     seen = {} 
-#     result = [] 
-#     for item in seq: 
-#         marker = idfun(item) 
-#         # in old Python versions: 
-#         # if seen.has_key(marker) 
-#         # but in new ones: 
-#         if marker in seen: continue 
-#         seen[marker] = 1 
-#         result.append(item) 
-#     return result
+def uniquify(seq, idfun=None):  
+    # order preserving 
+    if idfun is None: 
+        def idfun(x): return x 
+    seen = {} 
+    result = [] 
+    for item in seq: 
+        marker = idfun(item) 
+        # in old Python versions: 
+        # if seen.has_key(marker) 
+        # but in new ones: 
+        if marker in seen: continue 
+        seen[marker] = 1 
+        result.append(item) 
+    return result
 
 # unique_tags = uniquify(seq = all_tags)
 
