3f55ade130d2132b1c83b6208cdd0843178f34b4
unknown <miaomiaocui@miaomiaocui-PC.(none)>
2012-02-07 10:59:52 -0800

    Load user data

--- code/labor_force.R
+++ code/labor_force.R
@@ -2,3 +2,4 @@
 it_b <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/it_share_broad.csv",header=TRUE)
 it_n <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/it_share_narrow.csv",header=TRUE)
 labor <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/labor.csv",header=TRUE)
+user <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/users_geocoded_final.csv",header=TRUE)
