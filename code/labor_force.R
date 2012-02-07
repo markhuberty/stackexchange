171eeed51cefec4f589dcb56e217e4bea9ee5854
unknown <miaomiaocui@miaomiaocui-PC.(none)>
2012-02-06 23:36:03 -0800

    Add labor force, patent count and user analyze codes

---
+++ code/labor_force.R
@@ -0,0 +1,4 @@
+#load the files
+it_b <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/it_share_broad.csv",header=TRUE)
+it_n <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/it_share_narrow.csv",header=TRUE)
+labor <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/labor.csv",header=TRUE)
---
+++ code/patent.R
@@ -0,0 +1,2 @@
+#load the file
+patent <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/patents_count.csv",header=TRUE)
