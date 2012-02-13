5e45d78e5b372e20d8bd10d0f2dc8673632be092
unknown <miaomiaocui@miaomiaocui-PC.(none)>
2012-02-13 15:43:58 -0800

    descriptive stat plot

--- code/descriptive_stat.R
+++ code/descriptive_stat.R
@@ -5,35 +5,55 @@ user <- read.csv("C:/Users/miaomiaocui/Documents/test/stackexchange/data/users_g
 #take out user location
 user$Location <-NULL
 
+#assign binary values to website and dataframe it
+user$web.binary <- ifelse(is.na(user$WebsiteUrl),0,1)
+user.mean.web <- data.frame(tapply(user$web.binary,user$country.code,mean))
+
+
 #calculate basic statistics
-user.mean.rep <-data.frame(tapply(user$Reputation,user$country.code,mean))
-user.sd.rep <-data.frame(tapply(user$Reputation,user$country.code,sd))
-user.qu.rep<- data.frame(tapply(user$Reputation,user$country.code,quantile))
+user.mean.rep <- tapply(user$Reputation,user$country.code,mean)
+user.sd.rep <- tapply(user$Reputation,user$country.code,sd)
+user.qu.rep <- tapply(user$Reputation,user$country.code,quantile)
 
-user.mean.up <-data.frame(tapply(user$UpVotes,user$country.code,mean))
-user.sd.up <-data.frame(tapply(user$UpVotes,user$country.code,sd))
-user.qu.up<- data.frame(tapply(user$UpVotes,user$country.code,quantile))
+user.mean.up <- tapply(user$UpVotes,user$country.code,mean)
+user.sd.up <- tapply(user$UpVotes,user$country.code,sd)
+user.qu.up<-  tapply(user$UpVotes,user$country.code,quantile)
 
-user.mean.dn <-data.frame(tapply(user$DownVotes,user$country.code,mean))
-user.sd.dn <-data.frame(tapply(user$DownVotes,user$country.code,sd))
-user.qu.dn<- data.frame(tapply(user$DownVotes,user$country.code,quantile))
+user.mean.dn <-  tapply(user$DownVotes,user$country.code,mean)
+user.sd.dn <- tapply(user$DownVotes,user$country.code,sd)
+user.qu.dn<- tapply(user$DownVotes,user$country.code,quantile)
 
-#merge based on country code
-user.new <- merge(user.mean.rep,user.sd.rep,by="row.names",all=FALSE)
-user.new <- merge(user.new,user.qu.rep,by.x="Row.names",by.y="row.names",all=FALSE)
-user.new <- merge(user.new,user.mean.up,by.x="Row.names",by.y="row.names",all=FALSE)
-user.new <- merge(user.new,user.sd.up,by.x="Row.names",by.y="row.names",all=FALSE)
-user.new <- merge(user.new,user.qu.up,by.x="Row.names",by.y="row.names",all=FALSE)
-user.new <- merge(user.new,user.mean.dn,by.x="Row.names",by.y="row.names",all=FALSE)
-user.new <- merge(user.new,user.sd.dn,by.x="Row.names",by.y="row.names",all=FALSE)
-user.new <- merge(user.new,user.qu.dn,by.x="Row.names",by.y="row.names",all=FALSE)
 
-#rename columns
-names(user.new)<- c("country.code","mean.rep","sd.rep","qu.rep","mean.up","sd.up","qu.up",
-                    "mean.dn","sd.dn","qu.dn"
+user.new <- data.frame(unlist(user.mean.rep),
+                   unlist(user.sd.rep),
+#                    unlist(user.qu.rep),
+                   unlist(user.mean.up),
+                   unlist(user.sd.up),
+#                    unlist(user.qu.up),
+                   unlist(user.mean.dn),
+                   unlist(user.sd.dn)
+#                    unlist(user.qu.dn),    
                   )
 
+user.new$country.code <- rownames(user.new)
+names(user.new) <- c("mean.rep","sd.rep","mean.up","sd.up","mean.dn","sd.dn","country.code")
+
 #reshape
-user.new <- melt(user.new,id="country.code")
+user.new <- melt(user.new,id.vars="country.code")
+
+#ggplot
+library(ggplot2)
+library(hmisc)
+  
+
+plot.stat <- ggplot(user.new,
+       aes(x=country.code,y=value,group=variable))
+plot.stat+geom_point()+facet_grid(variable~.,scales="free")+
+         scale_x_discrete("Country")+
+         opts(strip.text.y=theme_text())
+      
+pdf("C:/Users/miaomiaocui/Documents/test/stackexchange/figures/user_stat_plot.pdf")
+
+
                                                        
 
