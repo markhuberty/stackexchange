fe888ba9541e9fa8fe8bcfb93a89f823e2ef7f47
Dongmiao Cui <miaomiaocui@miaomiaocui-PC.(none)>
2012-05-16 11:41:06 -0700

    merging question and answer into the same table

---
+++ code/question_answer_count.R
@@ -0,0 +1,14 @@
+library(reshape)
+library(foreach)
+library(stringr)
+library(ggplot2)
+library(Hmisc)
+library(gdata)
+
+
+#load question_answer_count data
+q.a.count <- read.csv("C:/Users/miaomiaocui/stackexchange/data/question_answer_counts.csv",header=TRUE)
+names(q.a.count) <- c("week","user","post.type","count")
+q<-drop.levels(q.a.count[q.a.count$post.type==1,])
+a<-drop.levels(q.a.count[q.a.count$post.type==2,])
+qa.count<-merge(q,a,by="week",all=TRUE)
